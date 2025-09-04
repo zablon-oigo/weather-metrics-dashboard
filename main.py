466f517e13d87d1c5832f9f23a387bd143d78d5e
zablon <zablonoigo51@gmail.com>
2025-09-04 22:00:14 +0300

    set max number of forecast

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import requests
 import os
 
-
 API_KEY = os.getenv("API_KEY")
 CITY = "Nairobi"
-BASE_URL = f"http://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={API_KEY}&units=metric"
+CNT = 16  
+BASE_URL = f"http://pro.openweathermap.org/data/2.5/forecast/daily?q={CITY}&cnt={CNT}&appid={API_KEY}&units=metric"
 
 def weather_data():
     response = requests.get(BASE_URL)
     if response.status_code == 200:
-        data = response.json()
-        return data
+        return response.json()
     else:
         return {"error": f"Failed to fetch data. Status code: {response.status_code}"}
 
 forecast = weather_data()
 print(forecast)
-
