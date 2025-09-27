7b70603d0af02fff1677015e4dc7a1d3994d8ac4
zablon <zablonoigo51@gmail.com>
2025-09-27 22:32:19 +0300

    add sample data for test

--- src/weather_metrics_dashboard/defs/assets.py
+++ src/weather_metrics_dashboard/defs/assets.py
@@ -1,5 +1,19 @@
+import pandas as pd
+
 import dagster as dg
+
+sample_data_file = "src/weather_metrics_dashboard/defs/data/sample_data.csv"
+processed_data_file = "src/weather_metrics_dashboard//defs/data/processed_data.csv"
 
 
 @dg.asset
-def assets(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
+def processed_data():
+
+    df = pd.read_csv(sample_data_file)
+
+    df["age_group"] = pd.cut(
+        df["age"], bins=[0, 30, 40, 100], labels=["Young", "Middle", "Senior"]
+    )
+
+    df.to_csv(processed_data_file, index=False)
+    return "Data loaded successfully"
